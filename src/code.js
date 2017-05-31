2db51805c8db53a46f130b00f68b1a5c5fede1d0
Scott Moss <willscottmoss@gmail.com>
2017-05-31 11:28:11 -0400

    chore

--- src/code.js
+++ src/code.js
@@ -1,20 +1,2 @@
 import Rx from 'rxjs/Rx'
 
-export const createObservable = (cb) => {
-  return Rx.Observable.create(observer => {
-    cb((e, v) => {
-      if (e) {
-        observer.error(e)
-      } else {
-        observer.next(v)
-        observer.complete()
-      }
-    })
-  })
-}
-
-export const mapData = (array = []) => {
-  return Rx.Observable.from(array)
-  .filter(n => !isNaN(parseFloat(n)) && isFinite(n))
-  .map(n => n * 2)
-}
